116ee52af1e25a5364742d35be5ed5b81eff7714
ingyu yoon <a>
2019-05-19 10:08:21 +0900

    updateUser 리팩토링
    User 클래스 추가

--- src/store/authReducer.js
+++ src/store/authReducer.js
@@ -3,10 +3,23 @@ import firebase from 'firebase'
 
 
 const UPDATE_USER = 'UPDATE_USER'
-export const updateUser = (user) => {
+export const updateUser = (firebaseUser) => {
+    const user = new User(firebaseUser);
     return {
         type: UPDATE_USER,
         payload: user,
+    }
+}
+
+class User {
+    constructor(firebaseUser) {
+        this.displayName = firebaseUser.displayName;
+        this.email = firebaseUser.email;
+        this.emailVerified = firebaseUser.emailVerified;
+        this.photoURL = firebaseUser.photoURL;
+        this.isAnonymous = firebaseUser.isAnonymous;
+        this.uid = firebaseUser.uid;
+        this.providerData = firebaseUser.providerData;
     }
 }
 
@@ -16,18 +29,9 @@ export const auth = () => {
         // 로그인, 로그아웃, 회원가입 시에 호출 된다. 
         // 각 상태가 변할 때 마다 state에 유저 정보를 수정하기 위해 
         // dispatch해야하는데 이를 위해 thunk를 활용하면 된다. 
-        firebase.auth().onAuthStateChanged(function (user) {
-            if (user) {
-                const newUser = {
-                    displayName: user.displayName,
-                    email: user.email,
-                    emailVerified: user.emailVerified,
-                    photoURL: user.photoURL,
-                    isAnonymous: user.isAnonymous,
-                    uid: user.uid,
-                    providerData: user.providerData
-                }
-                dispatch(updateUser(newUser));
+        firebase.auth().onAuthStateChanged(function (firebaseUser) {
+            if (firebaseUser) {
+                dispatch(updateUser(firebaseUser));
             } else {
                 dispatch(updateUser(null));
             }
